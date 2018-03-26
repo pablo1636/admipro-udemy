4e3150cc0e620ef7dcfff33395dba381239a0eb6
pablo1636 <pablo.tolosa@gmail.com>
2018-03-26 11:38:53 -0300

    Seccion 15 lista

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -9,6 +9,7 @@ import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { LoginGuardGuard } from '../services/service.index';
 import { ProfileComponent } from './profile/profile.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 
 
@@ -25,6 +26,8 @@ const pagesRoutes: Routes = [
             { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
             { path: 'account-settings', component: AccoutSettingsComponent, data: { titulo: 'Ajustes de Tema' } },
             { path: 'perfil', component: ProfileComponent, data: { titulo: 'Perfil de Usuario' } },
+            // Mantenimiento
+            { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Mantenimiento de usuarios' } },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
         ]
     }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -85,18 +85,24 @@ export class UsuarioService {
 
   }
 
-  actualizarUsuario(usuario: Usuario) {
+  actualizarUsuario( usuario: Usuario ) {
+
     let url = URL_SERVICIOS + '/usuario/' + usuario._id;
     url += '?token=' + this.token;
-    return this.http.put( url, usuario)
-      .map( (resp: any ) => {
-          // this.usuario = resp.usuario;
-          let usuarioDB: Usuario = resp.usuario;
-          this.guardarStorage( usuarioDB._id, this.token, usuarioDB);
-          swal('Usuario actualizado', usuario.nombre, 'success');
-          return true;
 
-      });
+    return this.http.put( url, usuario )
+                .map( (resp: any) => {
+
+                  if ( usuario._id === this.usuario._id ) {
+                    let usuarioDB: Usuario = resp.usuario;
+                    this.guardarStorage( usuarioDB._id, this.token, usuarioDB );
+                  }
+
+                  swal('Usuario actualizado', usuario.nombre, 'success' );
+
+                  return true;
+                });
+
   }
 cambiarImagen( archivo: File, id: string) {
   this._subirArchivoService.subirArchivo( archivo, 'usuarios', id)
@@ -107,6 +113,34 @@ cambiarImagen( archivo: File, id: string) {
     }).catch ( resp => {
       console.log(resp);
     });
+
+}
+
+cargarUsuarios( desde: number = 0 ) {
+
+  let url = URL_SERVICIOS + '/usuario?desde=' + desde;
+  return this.http.get( url );
+
+}
+
+buscarUsuarios( termino: string ) {
+
+  let url = URL_SERVICIOS + '/busqueda/coleccion/usuarios/' + termino;
+  return this.http.get( url )
+              .map( (resp: any) => resp.usuarios );
+
+}
+
+borrarUsuario( id: string ) {
+
+  let url = URL_SERVICIOS + '/usuario/' + id;
+  url += '?token=' + this.token;
+
+  return this.http.delete( url )
+              .map( resp => {
+                swal('Usuario borrado', 'El usuario a sido eliminado correctamente', 'success');
+                return true;
+              });
 
 }
 
