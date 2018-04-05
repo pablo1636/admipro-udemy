68d9493ef12202cc4ed3e5a9dd77593233349471
pablo1636 <pablo.tolosa@gmail.com>
2018-04-04 22:00:32 -0300

    Fin de la seccion 16

--- src/app/services/service.index.ts
+++ src/app/services/service.index.ts
@@ -5,4 +5,8 @@ export { SettingsService} from './settings/settings.service';
 export { SharedService } from './shared/shared.service';
 export { SidebarService } from './shared/sidebar.service';
 export { UsuarioService } from './usuario/usuario.service';
+export { HospitalService } from './hospital/hospital.service';
+export { MedicoService } from './medico/medico.service';
+
+
 
